28e72c670d0d8362041cab7452d6f5cd7bc46a50
Yoplayer-1 <66262055+Yoplayer-1@users.noreply.github.com>
2024-05-27 16:15:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // script.js
+const carouselContainer = document.querySelector('.carousel-container');
 const carousel = document.querySelector('.carousel');
 const imagesContainer = document.querySelector('.carousel-images');
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
+const zoomedImageContainer = document.querySelector('.zoomed-image-container');
+const zoomedImage = document.querySelector('.zoomed-image');
+const closeZoomBtn = document.querySelector('.close-zoom-btn');
 let currentIndex = 0;
 
 nextBtn.addEventListener('click', () => {
@@ -13,6 +17,18 @@ nextBtn.addEventListener('click', () => {
 prevBtn.addEventListener('click', () => {
   currentIndex = (currentIndex - 1 + imagesContainer.children.length) % imagesContainer.children.length;
   updateCarousel();
+});
+
+imagesContainer.addEventListener('click', (event) => {
+  if (event.target.tagName === 'IMG') {
+    const imgUrl = event.target.src;
+    zoomedImage.src = imgUrl;
+    zoomedImageContainer.style.display = 'flex';
+  }
+});
+
+closeZoomBtn.addEventListener('click', () => {
+  zoomedImageContainer.style.display = 'none';
 });
 
 function updateCarousel() {
